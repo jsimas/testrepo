1994529ba4f65a01f16aa89a76a26c7b74ea957c
Joao Simas <jsimas@gmail.com>
2019-02-18 09:34:13 +0100

    Some work done

--- src/main/java/hello/GreetingController.java
+++ src/main/java/hello/GreetingController.java
@@ -5,6 +5,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * Add some comments on the controller and explain what it does
+ */
 @RestController
 public class GreetingController {
 
@@ -13,6 +16,9 @@ public class GreetingController {
 
     @RequestMapping("/greeting")
     public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
+        if (name.equals("World")) {
+            System.out.println("Default value is being used");
+        }
         return new Greeting(counter.incrementAndGet(),
                             String.format(template, name));
     }
