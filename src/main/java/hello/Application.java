9f8ce8377505b82d5173af4450831231cd916183
Joao Simas <joao.SIMAS@swissquote.ch>
2019-03-05 13:17:52 +0100

    Finalzing feature 6

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Application {
 
+    //Commenting and finalizing feature 6
     public static void main(String[] args) {
         System.out.print("Adding some stupid log in the beginning"); //TODO should replace this with a logger
         SpringApplication.run(Application.class, args);
